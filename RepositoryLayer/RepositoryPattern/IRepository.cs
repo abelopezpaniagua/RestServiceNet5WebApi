c5977ea0f5ddbc2f8e249546336fe7cbcb3a04ca
Abel Lopez Paniagua <abel.lopez@jala-foundation.org>
2021-07-28 11:43:48 -0400

    implement repository layer for customer

---
+++ RepositoryLayer/RepositoryPattern/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.RepositoryPattern
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(int Id);
+        void Insert(T entity);
+        void SaveChanges();
+    }
+}
